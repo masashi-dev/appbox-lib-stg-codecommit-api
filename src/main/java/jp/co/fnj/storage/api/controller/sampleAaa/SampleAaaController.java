980e22506eb185312ff6742b3c69143d80e0f417
godai_kaga_voteras <godai.kaga@voteras.co.jp>
2019-11-08 15:29:36 +0900

    バリデーション実装

--- src/main/java/jp/co/fnj/storage/api/controller/sampleAaa/SampleAaaController.java
+++ src/main/java/jp/co/fnj/storage/api/controller/sampleAaa/SampleAaaController.java
@@ -29,6 +29,7 @@ public class SampleAaaController {
 	@Autowired
 	public Validator validator;
 
+
     /**
      * ユーザー情報取得
      *
@@ -40,34 +41,37 @@ public class SampleAaaController {
      */
     @RequestMapping("/sample1")
     public ResponseEntity<SampleAaaResponse> getUserInfo(
-    		@RequestParam(value="user_id", defaultValue="99999")
-    		String user_id
-	) throws IOException {
-
-    	// バリデーション
-    	SampleAaaRequest req = new SampleAaaRequest(user_id);
-    	Set<ConstraintViolation<SampleAaaRequest>> errorResult = validator.validate(req);
-    	if (0 < errorResult.size()) {
-    		throw new IOException();
-    	}
-
-    	// ユーザー情報を取得
-    	// {ここでDBやファイルから情報を取得する}
-    	// ユーザー情報を取得できなかった場合
-    	if (true) {
-    		// {404エラー等を返却}
-    	}
-    	
-    	// レスポンスを生成
-    	// 上記で取得した値などを使ってレスポンスを組み立てる
-    	SampleAaaResponse res = new SampleAaaResponse(req.getUser_id(), "田中", "太郎", 18);
+    		@RequestParam(value="user_id", defaultValue="99999") String user_id
+	) {
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("header1", "heaer1-value");
-        HttpStatus status = HttpStatus.OK;
 
-        // レスポンスを返却
-        return new ResponseEntity<>(res, headers, status);
+
+    	// リクエストパラメータをリクエストクラスに設定
+    	SampleAaaRequest req = new SampleAaaRequest(user_id);
+    	
+    	// バリデーションエラーがある場合はエラーを返却
+    	Set<ConstraintViolation<SampleAaaRequest>> errorResult = validator.validate(req);
+    	if (0 < errorResult.size()) {
+            return new ResponseEntity<>(headers, HttpStatus.BAD_REQUEST);
+    	}
+
+
+    	// ユーザー情報を取得
+    	// {ここでDBやファイルから情報を取得する}
+
+    	// ユーザー情報を取得できなかった場合はエラーを返却
+    	// ※if文の条件は見直して下さい
+    	if ("333".equals(req.getUser_id())) {
+    		return new ResponseEntity<>(headers, HttpStatus.NOT_FOUND);
+    	}
+    	
+
+    	// レスポンスを返却
+    	// 上記で取得した値などを使ってレスポンスを組み立てる
+    	SampleAaaResponse res = new SampleAaaResponse(req.getUser_id(), "田中", "太郎", 18);
+        return new ResponseEntity<>(res, headers, HttpStatus.OK);
 
     }
 
