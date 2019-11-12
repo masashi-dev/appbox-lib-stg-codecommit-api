0f8f2c2e9d2289bd01b12a0f530d2c7852d128aa
godai_kaga_voteras <godai.kaga@voteras.co.jp>
2019-11-12 11:11:38 +0900

    最新のメッセージ一覧の内容を反映

--- src/main/java/jp/co/fnj/storage/api/constant/Messages.java
+++ src/main/java/jp/co/fnj/storage/api/constant/Messages.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 public final class Messages {
 
   public static final ResourceBundle rb = ResourceBundle.getBundle("messages");
+  // 共通
   public static final String E00000 = rb.getString("E00000");
   public static final String E00001 = rb.getString("E00001");
   public static final String E00002 = rb.getString("E00002");
@@ -15,11 +16,87 @@ public final class Messages {
   public static final String E00005 = rb.getString("E00005");
   public static final String E00006 = rb.getString("E00006");
   public static final String E00007 = rb.getString("E00007");
+
+  // 認証
   public static final String E01001 = rb.getString("E01001");
+  public static final String E01002 = rb.getString("E01002");
+  public static final String E01003 = rb.getString("E01003");
+  public static final String E01004 = rb.getString("E01004");
+  public static final String E01005 = rb.getString("E01005");
+  public static final String E01006 = rb.getString("E01006");
+  public static final String E01007 = rb.getString("E01007");
+
+  // ファイル管理
   public static final String E02001 = rb.getString("E02001");
+  public static final String E02002 = rb.getString("E02002");
+  public static final String E02003 = rb.getString("E02003");
+  public static final String E02004 = rb.getString("E02004");
+  public static final String E02005 = rb.getString("E02005");
+  public static final String E02006 = rb.getString("E02006");
+  public static final String E02007 = rb.getString("E02007");
+  public static final String E02008 = rb.getString("E02008");
+  public static final String E02009 = rb.getString("E02009");
+  public static final String E02010 = rb.getString("E02010");
+  public static final String E02011 = rb.getString("E02011");
+  public static final String E02012 = rb.getString("E02012");
+  public static final String E02013 = rb.getString("E02013");
+  public static final String E02014 = rb.getString("E02014");
+  public static final String E02015 = rb.getString("E02015");
+  public static final String E02016 = rb.getString("E02016");
+  public static final String E02017 = rb.getString("E02017");
+  public static final String E02018 = rb.getString("E02018");
+  public static final String E02019 = rb.getString("E02019");
+  public static final String E02020 = rb.getString("E02020");
+  public static final String E02021 = rb.getString("E02021");
+  public static final String E02022 = rb.getString("E02022");
+  public static final String E02023 = rb.getString("E02023");
+  public static final String E02024 = rb.getString("E02024");
+  public static final String E02025 = rb.getString("E02025");
+  public static final String E02026 = rb.getString("E02026");
+  public static final String E02027 = rb.getString("E02027");
+  public static final String E02028 = rb.getString("E02028");
+  public static final String E02029 = rb.getString("E02029");
+  public static final String I02001 = rb.getString("I02001");
+
+  // フォルダ管理
   public static final String E03002 = rb.getString("E03002");
+  public static final String E03003 = rb.getString("E03003");
+  public static final String E03004 = rb.getString("E03004");
+  public static final String E03005 = rb.getString("E03005");
+  public static final String E03006 = rb.getString("E03006");
+  public static final String E03007 = rb.getString("E03007");
+  public static final String E03008 = rb.getString("E03008");
+  public static final String E03009 = rb.getString("E03009");
+  public static final String E03010 = rb.getString("E03010");
+  public static final String E03011 = rb.getString("E03011");
+  public static final String E03012 = rb.getString("E03012");
+  public static final String E03013 = rb.getString("E03013");
+  public static final String E03014 = rb.getString("E03014");
+  public static final String E03015 = rb.getString("E03015");
+  public static final String E03016 = rb.getString("E03016");
+  public static final String E03017 = rb.getString("E03017");
+  public static final String E03018 = rb.getString("E03018");
+  public static final String E03019 = rb.getString("E03019");
+
+  // お気に入り管理
   public static final String E04001 = rb.getString("E04001");
+  public static final String E04002 = rb.getString("E04002");
+  public static final String E04003 = rb.getString("E04003");
+  public static final String E04004 = rb.getString("E04004");
+  public static final String E04005 = rb.getString("E04005");
+  public static final String E04006 = rb.getString("E04006");
+  public static final String E04007 = rb.getString("E04007");
+  public static final String E04008 = rb.getString("E04008");
+
+  // 外部サービス管理
   public static final String E05001 = rb.getString("E05001");
+  public static final String E05002 = rb.getString("E05002");
+  public static final String E05003 = rb.getString("E05003");
+  public static final String E05004 = rb.getString("E05004");
+  public static final String E05005 = rb.getString("E05005");
+  public static final String E05006 = rb.getString("E05006");
+  public static final String E05007 = rb.getString("E05007");
+
 
   public String E00000() {
     return E00000;
@@ -57,19 +134,280 @@ public final class Messages {
     return E01001;
   }
 
+  public String E01002() {
+    return E01002;
+  }
+
+  public String E01003() {
+    return E01003;
+  }
+
+  public String E01004() {
+    return E01004;
+  }
+
+  public String E01005() {
+    return E01005;
+  }
+
+  public String E01006() {
+    return E01006;
+  }
+
+  public String E01007() {
+    return E01007;
+  }
+
   public String E02001() {
     return E02001;
+  }
+
+  public String E02002() {
+    return E02002;
+  }
+
+  public String E02003() {
+    return E02003;
+  }
+
+  public String E02004() {
+    return E02004;
+  }
+
+  public String E02005() {
+    return E02005;
+  }
+
+  public String E02006() {
+    return E02006;
+  }
+
+  public String E02007() {
+    return E02007;
+  }
+
+  public String E02008() {
+    return E02008;
+  }
+
+  public String E02009() {
+    return E02009;
+  }
+
+  public String E02010() {
+    return E02010;
+  }
+
+  public String E02011() {
+    return E02011;
+  }
+
+  public String E02012() {
+    return E02012;
+  }
+
+  public String E02013() {
+    return E02013;
+  }
+
+  public String E02014() {
+    return E02014;
+  }
+
+  public String E02015() {
+    return E02015;
+  }
+
+  public String E02016() {
+    return E02016;
+  }
+
+  public String E02017() {
+    return E02017;
+  }
+
+  public String E02018() {
+    return E02018;
+  }
+
+  public String E02019() {
+    return E02019;
+  }
+
+  public String E02020() {
+    return E02020;
+  }
+
+  public String E02021() {
+    return E02021;
+  }
+
+  public String E02022() {
+    return E02022;
+  }
+
+  public String E02023() {
+    return E02023;
+  }
+
+  public String E02024() {
+    return E02024;
+  }
+
+  public String E02025() {
+    return E02025;
+  }
+
+  public String E02026() {
+    return E02026;
+  }
+
+  public String E02027() {
+    return E02027;
+  }
+
+  public String E02028() {
+    return E02028;
+  }
+
+  public String E02029() {
+    return E02029;
+  }
+
+  public String I02001() {
+    return I02001;
   }
 
   public String E03002() {
     return E03002;
   }
 
+  public String E03003() {
+    return E03003;
+  }
+
+  public String E03004() {
+    return E03004;
+  }
+
+  public String E03005() {
+    return E03005;
+  }
+
+  public String E03006() {
+    return E03006;
+  }
+
+  public String E03007() {
+    return E03007;
+  }
+
+  public String E03008() {
+    return E03008;
+  }
+
+  public String E03009() {
+    return E03009;
+  }
+
+  public String E03010() {
+    return E03010;
+  }
+
+  public String E03011() {
+    return E03011;
+  }
+
+  public String E03012() {
+    return E03012;
+  }
+
+  public String E03013() {
+    return E03013;
+  }
+
+  public String E03014() {
+    return E03014;
+  }
+
+  public String E03015() {
+    return E03015;
+  }
+
+  public String E03016() {
+    return E03016;
+  }
+
+  public String E03017() {
+    return E03017;
+  }
+
+  public String E03018() {
+    return E03018;
+  }
+
+  public String E03019() {
+    return E03019;
+  }
+
   public String E04001() {
     return E04001;
+  }
+
+  public String E04002() {
+    return E04002;
+  }
+
+  public String E04003() {
+    return E04003;
+  }
+
+  public String E04004() {
+    return E04004;
+  }
+
+  public String E04005() {
+    return E04005;
+  }
+
+  public String E04006() {
+    return E04006;
+  }
+
+  public String E04007() {
+    return E04007;
+  }
+
+  public String E04008() {
+    return E04008;
   }
 
   public String E05001() {
     return E05001;
   }
+
+  public String E05002() {
+    return E05002;
+  }
+
+  public String E05003() {
+    return E05003;
+  }
+
+  public String E05004() {
+    return E05004;
+  }
+
+  public String E05005() {
+    return E05005;
+  }
+
+  public String E05006() {
+    return E05006;
+  }
+
+  public String E05007() {
+    return E05007;
+  }
+
 }
