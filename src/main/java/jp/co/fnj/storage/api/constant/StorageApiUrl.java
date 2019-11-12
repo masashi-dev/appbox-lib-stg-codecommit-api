674ea5ebcae95f973df40cf754d1f1c27e0bbab2
godai_kaga_voteras <godai.kaga@voteras.co.jp>
2019-11-12 12:02:50 +0900

    定数名を変更

--- src/main/java/jp/co/fnj/storage/api/constant/StorageApiUrl.java
+++ src/main/java/jp/co/fnj/storage/api/constant/StorageApiUrl.java
@@ -8,7 +8,7 @@ public final class StorageApiUrl {
   public static final String STORAGEACCESS = "/storageaccess";
 
   // 表示順登録
-  public static final String FILE_INSERT_DISPLAY_ORDER = "/lib/filemanagement/insert"; // TODO:適切なURLを指定する
+  public static final String SORT_ORDER_RESIST = "/lib/filemanagement/insert"; // TODO:適切なURLを指定する
   // 表示順更新
   public static final String SORT_ORDER_UPDATE = "/lib/filemanagement/update"; // TODO:URLが確定次第修正する
 }
