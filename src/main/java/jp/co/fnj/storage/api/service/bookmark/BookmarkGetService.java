df1e1e251421bba7d22ad5c4ea3539a5a5e2101c
ikuma sawano <ikuma.sawano@voteras.co.jp>
2019-11-13 18:28:28 +0900

    お気に入り取得 作成

--- src/main/java/jp/co/fnj/storage/api/service/bookmark/BookmarkGetService.java
+++ src/main/java/jp/co/fnj/storage/api/service/bookmark/BookmarkGetService.java
@@ -1,18 +1,18 @@
 package jp.co.fnj.storage.api.service.bookmark;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import jp.co.fnj.storage.api.entity.mapper.custom.BookmarkGetMapper;
 import jp.co.fnj.storage.api.entity.mapper.generat.TBookmarkMapper;
-import jp.co.fnj.storage.api.entity.model.generat.TBookmarkExample;
+import jp.co.fnj.storage.api.entity.model.custom.BookmarkGetEntity;
+import jp.co.fnj.storage.api.model.bookmark.BookmarkGetDetail;
 import jp.co.fnj.storage.api.model.bookmark.BookmarkGetRequest;
 import jp.co.fnj.storage.api.model.bookmark.BookmarkGetResponse;
-import jp.co.fnj.storage.api.model.file.FileGetListResponse;
 
 /**
  * お気に入り取得APIサービス.
@@ -25,14 +25,61 @@ public class BookmarkGetService<REQUEST_BODY extends BookmarkGetRequest, RESPONS
   /** お気に入りテーブルマッパー */
   @Autowired
   TBookmarkMapper tBookmarkMapper;
+  
+  /** お気に入り取得テーブルマッパー */
+  @Autowired
+  private BookmarkGetMapper bookmarkGetMapper;
 
   @Transactional(noRollbackFor = Throwable.class)
   public RESPONSE execute(HttpServletRequest request, HttpServletResponse response,
       REQUEST_BODY requestBody) {
+    //レスポンス用のデータを用意
+    BookmarkGetResponse responseBody = new BookmarkGetResponse();
+    List<BookmarkGetDetail> bookmarkList = new ArrayList<>();
     
-    BookmarkGetResponse responseBody = null;
+    String userId = "12345678901234"; //TODO: セッションからユーザーIDを取得する
     
-    // 全体を表示順にソートして返却
+    // お気に入りファイルとフォルダの情報取得
+    List<BookmarkGetEntity> bookmarkEntities = bookmarkGetMapper.selectBookmark(userId);
+
+    //リスト作成
+    for (BookmarkGetEntity bookmark : bookmarkEntities) {
+      BookmarkGetDetail detail = new BookmarkGetDetail();
+      detail.setBookmark_id(bookmark.getBookmarkId());
+      //ファイル情報格納
+      if (bookmark.getTFile() != null) {
+        detail.setFile_file_id(bookmark.getTFile().getFileId());
+        detail.setFile_folder_id(bookmark.getTFile().getFolderId());
+        detail.setFile_file_name(bookmark.getTFile().getFileName());
+        detail.setFile_s3_file_name(bookmark.getTFile().getS3FileName());
+        detail.setFile_s3_objecrt_key(bookmark.getTFile().getS3ObjectKey());
+        detail.setFile_file_size(bookmark.getTFile().getFileSize());
+        detail.setFile_insert_user_id(bookmark.getTFile().getCreateUser());
+        detail.setFile_insert_date_time(bookmark.getTFile().getCreateDate());
+        detail.setFile_update_user_id(bookmark.getTFile().getUpdateUser());
+        detail.setFile_update_date_time(bookmark.getTFile().getUpdateDate());
+        bookmarkList.add(detail);
+      }
+      //フォルダ情報格納
+      else if (bookmark.getTFolder() != null) {
+        detail.setFolder_folder_id(bookmark.getTFolder().getFolderId());
+        detail.setFolder_parent_folder_id(bookmark.getTFolder().getParentFolderId());
+        detail.setFolder_folder_name(bookmark.getTFolder().getFolderName());
+        detail.setFolder_s3_folder_name(bookmark.getTFolder().getS3FolderName());
+        detail.setFolder_developer_id(bookmark.getTFolder().getDeveloperId());
+        detail.setFolder_mansion_id(bookmark.getTFolder().getMansionId());
+        detail.setFolder_s3_object_key(bookmark.getTFolder().getS3ObjectKey());
+        detail.setFolder_explanatory_text(bookmark.getTFolder().getExplanatoryText());
+        detail.setFolder_insert_user_id(bookmark.getTFolder().getCreateUser());
+        detail.setFolder_insert_date_time(bookmark.getTFolder().getCreateDate());
+        detail.setFolder_update_user_id(bookmark.getTFolder().getUpdateUser());
+        detail.setFolder_update_date_time(bookmark.getTFolder().getUpdateDate());
+        bookmarkList.add(detail);
+      }
+    }
+    // 抽出された結果をレスポンスListに登録
+    responseBody.setBookmark_list(bookmarkList);
+    responseBody.setUser_id(userId);
     return (RESPONSE) responseBody;
   }
 }
