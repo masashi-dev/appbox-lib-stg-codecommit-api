cc4783c92197382e04a0d99cc71396eff5cf82ef
kohei-hashimoto_voteras <kohei.hashimoto@voteras.co.jp>
2019-11-14 13:28:03 +0900

    全文検索のIF作成

---
+++ src/main/java/jp/co/fnj/storage/api/controller/file/FileFullTextSearchController.java
@@ -0,0 +1,45 @@
+package jp.co.fnj.storage.api.controller.file;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import jp.co.fnj.storage.api.logic.file.FileFullTextSearchLogic;
+import jp.co.fnj.storage.api.model.file.FileFullTextSearchRequest;
+import jp.co.fnj.storage.api.model.file.FileFullTextSearchResponse;
+
+/**
+ * ファイル全文検索APIコントローラ.
+ *
+ */
+@RestController
+@Validated
+public class FileFullTextSearchController {
+
+  @Autowired
+  FileFullTextSearchLogic<FileFullTextSearchRequest, List<FileFullTextSearchResponse>> fileFullTextSearchLogic;
+
+  /**
+   * ファイル全文検索.
+   *
+   * 指定した入力を元にファイル名、ファイルの全文検索を行う。
+   *
+   * @param folder_id フォルダID
+   * @param search_word 検索ワード
+   * @return FileFullTextSearchResponse ファイル検索結果
+   */
+  @RequestMapping(method = RequestMethod.GET, path = "/fulltextsearch") // TODO:エンドポイントは確定後書き換えること
+  public ResponseEntity<List<FileFullTextSearchResponse>> search(HttpServletRequest request,
+      HttpServletResponse response,
+      @ModelAttribute @Validated FileFullTextSearchRequest requestBody) {
+
+    return fileFullTextSearchLogic.execute(request, response, requestBody);
+  }
+
+}
---
+++ src/main/java/jp/co/fnj/storage/api/model/file/FileFullTextSearchRequest.java
@@ -0,0 +1,27 @@
+package jp.co.fnj.storage.api.model.file;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * ファイル全文検索APIリクエスト.
+ *
+ */
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+public class FileFullTextSearchRequest {
+
+  /** フォルダID */
+  @NotNull
+  @Size(max = 14)
+  private String folder_id;
+
+  /** 検索ワード */
+  @NotNull
+  @Size(min = 1, max = 50)
+  private String search_word;
+}
